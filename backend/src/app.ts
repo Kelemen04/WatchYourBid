fa59527e2ad4cf2b7523d10dd184e31068cc23cf
Szilard <szilardkelemen12@gmail.com>
2026-03-03 19:44:04 +0200

    implement docker and prisma

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,17 @@
 import express from 'express';
 
 const app = express();
-const port = 3000;
+const port = 8000;
+
+app.get('/', (req,res) => {
+    res.send("Helllo");
+}) 
+
+app.get('/:id', (req,res) => {
+    console.log(`${JSON.stringify(req.query)}`)
+    console.log(`${JSON.stringify(req.headers)}`)
+    res.send(`Sziaa ${req.params.id}`);
+})
 
 app.listen(port, () => {
     console.log(`Server listening on port - ${port}`);
